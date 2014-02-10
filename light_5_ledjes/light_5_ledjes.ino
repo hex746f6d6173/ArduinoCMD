8b271138b14b20d847a1286d90a38e324c3705c4
Tomas Harkema <info@tomasharkema.nl>
2014-02-10 23:13:57 +0100

    Set pinmode

--- light_5_ledjes/light_5_ledjes.ino
+++ light_5_ledjes/light_5_ledjes.ino
@@ -12,7 +12,12 @@ int LDR = 0;
 void setup()
   {
 
-    //SETUP SHIT
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(led3, OUTPUT);
+    pinMode(led4, OUTPUT);
+    pinMode(led5, OUTPUT);
+    
   }
  
 void loop()
