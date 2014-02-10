0fe187d5dfbe578ffce15ef0328b4654b7514491
pronks001 <pronks001@gmail.com>
2014-02-10 23:50:15 +0100

    Nieuwste versie LDR_ledjes online
    Oude versie verwijdert,
    Nieuwe versie toegevoegd...

--- light_5_ledjes/LDR_ledjes.ino
+++ light_5_ledjes/LDR_ledjes.ino
@@ -6,29 +6,28 @@ int led4 = 7;
 int led5 = 5;
 
 int val = 0;
-
-int LDR = 0;
+int LDR = A0;
 
 void setup()
   {
-  
-    Serial.begin(9600);
+     Serial.begin(9600);
     
-    pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
-    pinMode(led3, OUTPUT);
-    pinMode(led4, OUTPUT);
-    pinMode(led5, OUTPUT);
-    
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(5, OUTPUT);
+    //SETUP SHIT
   }
  
 void loop()
   {
+    
     int val = analogRead(LDR);          //reads the ldr’s value through LDR which we have set to Analog input 0 “A0″
    
-    val = map(val, 20, 36, 1, 6);
+    val = map(val, 8, 30, 0, 5);
    
-    Serial.print(analogRead(LDR));
+   Serial.print(analogRead(LDR));
     Serial.print(", ");
     Serial.println(val);
    
