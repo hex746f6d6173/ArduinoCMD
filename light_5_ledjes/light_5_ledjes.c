aeb06969c8ef69ecc4737bab196b00c1ca512ce9
Tomas Harkema <info@tomasharkema.nl>
2014-02-12 17:19:14 +0100

    Should be C

---
+++ light_5_ledjes/light_5_ledjes.c
@@ -0,0 +1,52 @@
+
+int led1 = 13;
+int led2 = 11;
+int led3 = 9;
+int led4 = 7;
+int led5 = 5;
+
+int val = 0;
+
+void setup()
+  {
+
+    //SETUP SHIT
+  }
+ 
+void loop()
+  {
+    int val = analogRead(LDR);          //reads the ldr’s value through LDR which we have set to Analog input 0 “A0″
+   
+    val = map(val, 20, 36, 1, 6);
+   
+    if(led1 < 1){
+      digitalWrite(led1, HIGH); 
+    }else{
+      digitalWrite(led1, LOW); 
+    }
+    
+    if(led2 < 2){
+      digitalWrite(led2, HIGH); 
+    }else{
+      digitalWrite(led2, LOW); 
+    }
+    
+    if(led3 < 3){
+      digitalWrite(led3, HIGH); 
+    }else{
+      digitalWrite(led3, LOW); 
+    }
+    
+    if(led4 < 4){
+      digitalWrite(led4, HIGH); 
+    }else{
+      digitalWrite(led4, LOW); 
+    }
+    
+    if(led5 < 5){
+      digitalWrite(led5, HIGH); 
+    }else{
+      digitalWrite(led5, LOW); 
+    }
+   
+  }
